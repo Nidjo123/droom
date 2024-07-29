2a3ad34ea4a59eba0c7e3aeb3bf91d9d482abbe2
Nikola Bunjevac <nikola.bunjevac@gmail.com>
2024-07-29 17:19:20 +0200

    Create window

--- main.cpp
+++ main.cpp
@@ -1,7 +1,36 @@
-#include <iostream>
 #include <SDL.h>
 
+const int WIDTH = 800;
+const int HEIGHT = 600;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    SDL_Init(SDL_INIT_EVERYTHING);
+
+    SDL_Window *window = SDL_CreateWindow("Droom", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, 0);
+    if (!window) {
+        SDL_LogCritical(SDL_LOG_CATEGORY_APPLICATION, "Failed to create window: %s\n", SDL_GetError());
+        return -1;
+    }
+
+    bool running = true;
+    while (running) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+                case SDL_QUIT:
+                    running = false;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        SDL_Delay(1);
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
     return 0;
 }
